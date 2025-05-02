1159b6c56d0df4afb077c26017cc2d996ad26903
Soren <danilo-258@hotmail.com>
2025-05-02 14:35:41 -0300

    feat(shopee): atualizar lista de produtos Shopee com novos itens, ajustar paths de imagens e links de afiliados

--- src/assets/js/shopee.js
+++ src/assets/js/shopee.js
@@ -1,5 +1,26 @@
 export const productsShopee = [
-  { id: "S1", name: "Fone Bluetooth TWS",   image: "../image/shopee/tws-earbuds.jpg",    affiliateLink: "https://shope.ee/exemplo1" },
-  { id: "S2", name: "Smartwatch X9 Pro",    image: "../image/shopee/smartwatch-x9.jpg", affiliateLink: "https://shope.ee/exemplo2" }
-  // ...
+  { 
+    id: "1", 
+    name: "Projetor HY 300",   
+    image: "../image/produtos/shopee/PRODUTO_N_1.jpg",    
+    affiliateLink: "https://s.shopee.com.br/6VAyL0dq4H" 
+  },
+  { 
+    id: "2", 
+    name: "Fone De Ouvido bluetooth 5.1 M100",    
+    image: "../image/produtos/shopee/PRODUTO_N_2.webp", 
+    affiliateLink: "https://s.shopee.com.br/9f80Lb1dJn" 
+  },
+  { 
+    id: "3", 
+    name: "Mini Liquidificador Portátil",   
+    image: "../image/produtos/shopee/PRODUTO_N_3.webp",    
+    affiliateLink: "https://s.shopee.com.br/9zkqkX15JO" 
+  },
+  { 
+    id: "4", 
+    name: "Fone De Ouvido Sem Fio E6s Bluetooth 5.0 Tws",   
+    image: "../image/produtos/shopee/PRODUTO_N_4.webp",    
+    affiliateLink: "https://s.shopee.com.br/1B9SESTzAE" 
+  },
 ];
