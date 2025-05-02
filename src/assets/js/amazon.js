610fe1765d30609a1988bc76fd8d1d9024c07fbf
Soren <danilo-258@hotmail.com>
2025-05-02 11:51:08 -0300

    Add productsAmazon data with initial product entries

---
+++ src/assets/js/amazon.js
@@ -0,0 +1,5 @@
+export const productsAmazon = [
+  { id: "A1", name: "Echo Dot (4ª Geração)", image: "../image/amazon/echo-dot-4th.jpg", affiliateLink: "https://amzn.to/exemplo1" },
+  { id: "A2", name: "Fire TV Stick 4K",       image: "../image/amazon/fire-tv-stick-4k.jpg", affiliateLink: "https://amzn.to/exemplo2" }
+  // ...
+];
