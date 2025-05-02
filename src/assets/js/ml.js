b362c093c355712bfa2c10e1902095010c5c9e36
Soren <danilo-258@hotmail.com>
2025-05-02 12:46:12 -0300

    feat: adicionando produto

--- src/assets/js/ml.js
+++ src/assets/js/ml.js
@@ -1,5 +1,10 @@
 export const productsML = [
-  { id: "ML1", name: "Smartphone Samsung Galaxy A54", image: "../image/ml/galaxy-a54.jpg", affiliateLink: "https://mercadolivre.com/exemplo1" },
-  { id: "ML2", name: "Notebook Dell Inspiron 15",     image: "../image/ml/dell-inspiron.jpg", affiliateLink: "https://mercadolivre.com/exemplo2" }
+  { 
+    id: "1", 
+    name: "Coberta Manta Soft Casal Microfibra Veludo", 
+    image: "../image/produtos/mercado_livre/PRODUTO_N_1-Coberta_Manta_Soft_Casal_Microfibra_Veludo.webp",
+    affiliateLink: "https://mercadolivre.com/exemplo1" 
+  },
+  { id: "2", name: "Notebook Dell Inspiron 15",     image: "../image/ml/dell-inspiron.jpg", affiliateLink: "https://mercadolivre.com/exemplo2" }
   // ...
 ];
