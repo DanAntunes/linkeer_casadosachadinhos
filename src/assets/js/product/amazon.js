3963ebaefd51a8008ab121ca16465a25126f81b7
Soren <danilo-258@hotmail.com>
2025-05-14 17:47:05 -0300

    feat(amazon): adicionar novos produtos IDs 12–18
    - Inclui “O despertar das monções: A Guerra dos Furacões - vol. 2” (ID 12)
    - Inclui “Bíblia Clássica do Tarot” (ID 13)
    - Inclui “BOX As Crônicas de Nárnia - Edição de Luxo” (ID 14)
    - Inclui “Floriografia: A Linguagem Secreta das Flores” (ID 15)
    - Inclui “A paciente silenciosa” (ID 16)
    - Inclui “Liquidificador Turbo Power, Mondial, 550W” (ID 17)
    - Inclui “Imperfeitos” (ID 18)
    - Atualiza array `productsAmazon` com novas entradas para ampliar catálogo de produtos

--- src/assets/js/product/amazon.js
+++ src/assets/js/product/amazon.js
@@ -47,23 +47,65 @@ export const productsAmazon = [
     image: "../image/produtos/amazon/PRODUTO_N_8.jpg", 
     affiliateLink: "https://amzn.to/3GJqF2g" 
   },
-    { 
+  { 
     id: "9", 
     name: "Nunca minta Capa comum",       
     image: "../image/produtos/amazon/PRODUTO_N_9.jpg", 
     affiliateLink: "https://amzn.to/4k5b4It" 
   },
-    { 
+  { 
     id: "10", 
     name: "O despertar da lua caída – Edição especial em capa dura",       
     image: "../image/produtos/amazon/PRODUTO_N_10.jpg", 
     affiliateLink: "https://amzn.to/4k9VImc" 
   },
-    { 
+  { 
     id: "11", 
     name: "Pipoqueira, Pop Time, Branco, 220v, Britânia",       
     image: "../image/produtos/amazon/PRODUTO_N_11.jpg", 
     affiliateLink: "https://amzn.to/3RTX8oM" 
+  },
+  { 
+    id: "12", 
+    name: "O despertar das monções: A Guerra dos Furacões - vol. 2",       
+    image: "../image/produtos/amazon/PRODUTO_N_12.png", 
+    affiliateLink: "https://amzn.to/3FceWsv" 
+  },
+  { 
+    id: "13", 
+    name: "Bíblia Clássica do Tarot",       
+    image: "../image/produtos/amazon/PRODUTO_N_13.jpg", 
+    affiliateLink: "https://amzn.to/3F1KjpJ" 
+  },
+  { 
+    id: "14", 
+    name: "BOX As Crônicas de Nárnia - Edição de Luxo",       
+    image: "../image/produtos/amazon/PRODUTO_N_14.jpg", 
+    affiliateLink: "https://amzn.to/43h2OOS" 
+  },
+  { 
+    id: "15", 
+    name: "Floriografia: A Linguagem Secreta das Flores",       
+    image: "../image/produtos/amazon/PRODUTO_N_15.jpg", 
+    affiliateLink: " https://amzn.to/4kjiqs8" 
+  },
+  { 
+    id: "16", 
+    name: "A paciente silenciosa ",       
+    image: "../image/produtos/amazon/PRODUTO_N_16.jpg", 
+    affiliateLink: "https://amzn.to/44IWFgZ" 
+  },
+  { 
+    id: "17", 
+    name: "Liquidificador Turbo Power, Mondial, Preto, 550W, 110V - L-99 FB",       
+    image: "../image/produtos/amazon/PRODUTO_N_17.jpg", 
+    affiliateLink: "https://amzn.to/3ZleqiD" 
+  },
+  { 
+    id: "18", 
+    name: "Imperfeitos",       
+    image: "../image/produtos/amazon/PRODUTO_N_18.jpg", 
+    affiliateLink: "https://amzn.to/3GQ06Z5" 
   },
   
 ];
