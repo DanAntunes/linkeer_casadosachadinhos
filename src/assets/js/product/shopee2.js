43396800830369d080a9d02de45b2c44fa71cb49
Soren <danilo-258@hotmail.com>
2025-06-11 15:58:33 -0300

    feat(shopee): add product details for ID 53

--- src/assets/js/product/shopee2.js
+++ src/assets/js/product/shopee2.js
@@ -11,4 +11,10 @@ export const productsShopee2 = [
     image: "../image/produtos/shopee/shopee2/PRODUTO_N_52.webp",    
     affiliateLink: "https://s.shopee.com.br/9zluBxqsD3" 
   },
+  { 
+    id: "53", 
+    name: "Almofada Personalizada Namorados Com Suas Fotos",   
+    image: "../image/produtos/shopee/shopee2/PRODUTO_N_53.webp",    
+    affiliateLink: "https://s.shopee.com.br/6VC23Y6V63" 
+  },
 ]
