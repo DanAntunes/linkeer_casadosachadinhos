1751e886b334648edf07f59c2e09ebab30872ff5
Soren <danilo-258@hotmail.com>
2025-06-11 15:23:59 -0300

    feat(shopee): add product details for ID 51

--- src/assets/js/product/shopee2.js
+++ src/assets/js/product/shopee2.js
@@ -1,8 +1,8 @@
 export const productsShopee2 = [
   { 
-    id: "", 
-    name: "",   
-    image: "../image/produtos/shopee/shopee2/",    
-    affiliateLink: "" 
+    id: "51", 
+    name: "Pulseira Magnética De Coração",   
+    image: "../image/produtos/shopee/shopee2/PRODUTO_N_51.webp",    
+    affiliateLink: "https://s.shopee.com.br/5fcv1HuPkN" 
   },
 ]
