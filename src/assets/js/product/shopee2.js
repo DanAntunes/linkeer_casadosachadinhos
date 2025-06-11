471359080b41222f5ac9b08abf1b152f99e78aeb
Soren <danilo-258@hotmail.com>
2025-06-11 15:26:46 -0300

    feat(shopee): add product details for ID 52

--- src/assets/js/product/shopee2.js
+++ src/assets/js/product/shopee2.js
@@ -5,4 +5,10 @@ export const productsShopee2 = [
     image: "../image/produtos/shopee/shopee2/PRODUTO_N_51.webp",    
     affiliateLink: "https://s.shopee.com.br/5fcv1HuPkN" 
   },
+  { 
+    id: "52", 
+    name: "Colar Relicário Coração Com Duas Fotos",   
+    image: "../image/produtos/shopee/shopee2/PRODUTO_N_52.webp",    
+    affiliateLink: "https://s.shopee.com.br/9zluBxqsD3" 
+  },
 ]
