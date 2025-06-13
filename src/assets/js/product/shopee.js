44bbd727aacf824927f7c2399d191390f04f7ab6
Soren <danilo-258@hotmail.com>
2025-06-13 15:20:38 -0300

    feat(shopee): update image paths for multiple products

--- src/assets/js/product/shopee.js
+++ src/assets/js/product/shopee.js
@@ -14,61 +14,61 @@ export const productsShopee = [
   { 
     id: "3", 
     name: "Mini Liquidificador Portátil Shake",   
-    image: "../image/produtos/shopee/PRODUTO_N_3.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_3.webp",    
     affiliateLink: "https://s.shopee.com.br/9zkqkX15JO" 
   },
   { 
     id: "4", 
     name: "Fone De Ouvido Sem Fio E6s Bluetooth 5.0 Tws",   
-    image: "../image/produtos/shopee/PRODUTO_N_4.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_4.webp",    
     affiliateLink: "https://s.shopee.com.br/1B9SESTzAE" 
   },
   { 
     id: "5", 
     name: "Sandália Feminina Lumiss Salto Baixo Geométrico Metalizado",   
-    image: "../image/produtos/shopee/PRODUTO_N_5.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_5.webp",    
     affiliateLink: "https://s.shopee.com.br/4L6aM6Iubd" 
   },
   { 
     id: "6", 
     name: "Smartphone Infinix Note 40 5G Racing Edition",   
-    image: "../image/produtos/shopee/PRODUTO_N_6.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_6.webp",    
     affiliateLink: "https://s.shopee.com.br/7V3c8TtPEe" 
   },
   { 
     id: "7", 
     name: "Pro 4s TWS / i12 TWS Fone De Ouvido Sem Fio inpods 3",   
-    image: "../image/produtos/shopee/PRODUTO_N_7.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_7.webp",    
     affiliateLink: "https://s.shopee.com.br/1B9YcODFR1" 
   },
   { 
     id: "8", 
     name: "Comedouro Bebedouro Gato e Cachorro Elevado Pet Mdf",   
-    image: "../image/produtos/shopee/PRODUTO_N_8.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_8.webp",    
     affiliateLink: "https://s.shopee.com.br/7V3myhLJNS" 
   },
   { 
     id: "9", 
     name: "Camisa Social Floral Masculina Havaiana Estampa - Preta",   
-    image: "../image/produtos/shopee/PRODUTO_N_9.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_9.webp",    
     affiliateLink: "https://s.shopee.com.br/30bPPk5Vwe" 
   },
   { 
     id: "9", 
     name: "Camisa Social Floral Masculina Havaiana Estampa - Branca",   
-    image: "../image/produtos/shopee/PRODUTO_N_9_2.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_9_2.webp",    
     affiliateLink: "https://s.shopee.com.br/30bPPk5Vwe" 
   },
   { 
     id: "9", 
     name: "Camisa Social Floral Masculina Havaiana Estampa - Azul",   
-    image: "../image/produtos/shopee/PRODUTO_N_9_3.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_9_3.webp",    
     affiliateLink: "https://s.shopee.com.br/30bPPk5Vwe" 
   },
   { 
     id: "10", 
     name: "Videogame Stick 10mil jogos",   
-    image: "../image/produtos/shopee/PRODUTO_N_10.webp",    
+    image: "../image/produtos/shopee/shopee1/PRODUTO_N_10.webp",    
     affiliateLink: "https://s.shopee.com.br/4VQDEYaUsB" 
   },
   { 
