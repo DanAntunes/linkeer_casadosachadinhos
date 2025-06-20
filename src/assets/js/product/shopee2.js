cf728eaeb2801c80f79c030e42ea280159c5fa25
Soren <danilo-258@hotmail.com>
2025-06-19 21:00:22 -0300

    feat(shopee): update image paths for products 51-59

--- src/assets/js/product/shopee2.js
+++ src/assets/js/product/shopee2.js
@@ -2,55 +2,55 @@ export const productsShopee2 = [
   { 
     id: "51", 
     name: "Pulseira Magnética De Coração",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_51.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_51.webp",    
     affiliateLink: "https://s.shopee.com.br/5fcv1HuPkN" 
   },
   { 
     id: "52", 
     name: "Colar Relicário Coração Com Duas Fotos",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_52.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_52.webp",    
     affiliateLink: "https://s.shopee.com.br/9zluBxqsD3" 
   },
   { 
     id: "53", 
     name: "Almofada Personalizada Namorados Com Suas Fotos",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_53.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_53.webp",    
     affiliateLink: "https://s.shopee.com.br/6VC23Y6V63" 
   },
   { 
     id: "54", 
     name: "Jóias De Aniversário 3D Atmosférica Do Amor",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_54.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_54.webp",    
     affiliateLink: "https://s.shopee.com.br/1LTvuNe9rX" 
   },
   { 
     id: "55", 
     name: "Luminaria elegante decoração 3d saturno",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_55.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_55.webp",    
     affiliateLink: "https://s.shopee.com.br/4L7Ya22keD" 
   },
   { 
     id: "56", 
     name: "Luminaria lua 3d toque usb 5 cores RGB",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_56.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_56.webp",    
     affiliateLink: "https://s.shopee.com.br/5L05m8wTdq" 
   },
   { 
     id: "57", 
     name: "Lindo San Liou Kulomi Série Pequena Noite",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_57.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_57.webp",    
     affiliateLink: "https://s.shopee.com.br/1LTx12eqLX" 
   },
   { 
     id: "58", 
     name: "Luminária Capivara PLA Luz Noturna",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_58.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_58.webp",    
     affiliateLink: "https://s.shopee.com.br/8fGXknjaUF" 
   },
   { 
     id: "59", 
     name: "Penteadeira suspensa com divisória",   
-    image: "../image/produtos/shopee/shopee2/PRODUTO_N_59.webp",    
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_59.webp",    
     affiliateLink: "https://s.shopee.com.br/8fGXqwNkB2" 
   },
   { 
