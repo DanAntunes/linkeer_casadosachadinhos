4768ad8e0ed34b64359281dcfee27a921fd29ba4
Soren <danilo-258@hotmail.com>
2025-06-24 11:13:21 -0300

    fix(amazon): update image paths for products 1 to 3

--- src/assets/js/product/amazon.js
+++ src/assets/js/product/amazon.js
@@ -2,19 +2,19 @@ export const productsAmazon = [
   { 
     id: "1", 
     name: "Creamy Skincare Calming Cream", 
-    image: "../image/produtos/amazon/PRODUTO_N_1.jpg", 
+    image: "../image/produtos/amazon/1-50/PRODUTO_N_1.jpg", 
     affiliateLink: "https://amzn.to/4iSma2Q" 
   },
   { 
     id: "2", 
     name: "Luminária de Mesa LED Recarregável",       
-    image: "../image/produtos/amazon/PRODUTO_N_2.jpg", 
+    image: "../image/produtos/amazon/1-50/PRODUTO_N_2.jpg", 
     affiliateLink: "https://amzn.to/4jzBQca" 
   },
   { 
     id: "3", 
     name: "Secador de Cabelos Travel",       
-    image: "../image/produtos/amazon/PRODUTO_N_3.jpg", 
+    image: "../image/produtos/amazon/1-50/PRODUTO_N_3.jpg", 
     affiliateLink: "https://amzn.to/4jDtb8K" 
   },
   { 
