999e52558b5ea158ed9ff2027481b2bcbb837440
Soren <danilo-258@hotmail.com>
2025-07-01 23:11:22 -0300

    feat(shopee): update image paths for products 1 to 5

--- src/assets/js/product/shopee.js
+++ src/assets/js/product/shopee.js
@@ -2,31 +2,31 @@ export const productsShopee = [
   { 
     id: "1", 
     name: "Projetor HY 300",   
-    image: "../image/produtos/shopee/shopee1/PRODUTO_N_1.jpg",    
+    image: "../image/produtos/shopee/1-50/PRODUTO_N_1.jpg",    
     affiliateLink: "https://s.shopee.com.br/6VAyL0dq4H" 
   },
   { 
     id: "2", 
     name: "Fone De Ouvido bluetooth 5.1 M100",    
-    image: "../image/produtos/shopee/shopee1/PRODUTO_N_2.webp", 
+    image: "../image/produtos/shopee/1-50/PRODUTO_N_2.webp", 
     affiliateLink: "https://s.shopee.com.br/9f80Lb1dJn" 
   },
   { 
     id: "3", 
     name: "Mini Liquidificador Portátil Shake",   
-    image: "../image/produtos/shopee/shopee1/PRODUTO_N_3.webp",    
+    image: "../image/produtos/shopee/1-50/PRODUTO_N_3.webp",    
     affiliateLink: "https://s.shopee.com.br/9zkqkX15JO" 
   },
   { 
     id: "4", 
     name: "Fone De Ouvido Sem Fio E6s Bluetooth 5.0 Tws",   
-    image: "../image/produtos/shopee/shopee1/PRODUTO_N_4.webp",    
+    image: "../image/produtos/shopee/1-50/PRODUTO_N_4.webp",    
     affiliateLink: "https://s.shopee.com.br/1B9SESTzAE" 
   },
   { 
     id: "5", 
     name: "Sandália Feminina Lumiss Salto Baixo Geométrico Metalizado",   
-    image: "../image/produtos/shopee/shopee1/PRODUTO_N_5.webp",    
+    image: "../image/produtos/shopee/1-50/PRODUTO_N_5.webp",    
     affiliateLink: "https://s.shopee.com.br/4L6aM6Iubd" 
   },
   { 
