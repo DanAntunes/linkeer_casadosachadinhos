77aee8d10fe9c4c72b4d1e3dc2ba918633749efd
Soren <danilo-258@hotmail.com>
2025-07-03 13:21:56 -0300

    feat(shopee): add productsShopee3 to the product list

---
+++ src/assets/js/product/shopee3.js
@@ -0,0 +1,32 @@
+export const productsShopee3 = [
+  { 
+    id: "100", 
+    name: "Mesa Gamer Escrivaninha Para Computador",   
+    image: "../image/produtos/shopee/51-100/PRODUTO_N_100.webp",    
+    affiliateLink: "https://s.shopee.com.br/1Vnuono0oa" 
+  },
+  { 
+    id: "101", 
+    name: "Cadeira Escritório Gamer Ergonômica Reclinável",   
+    image: "../image/produtos/shopee/101-150/PRODUTO_N_101.webp",    
+    affiliateLink: "https://s.shopee.com.br/8AKoluu1JY" 
+  },
+    { 
+    id: "102", 
+    name: "Teclado Gamer/Escritório Com Fio RGB LED ",   
+    image: "../image/produtos/shopee/101-150/PRODUTO_N_102.webp",    
+    affiliateLink: "https://s.shopee.com.br/4fkwc9lyko" 
+  },
+    { 
+    id: "103", 
+    name: "Mouse Gamer Para Jogo 2.400 DPI Óptico com LED RGB",   
+    image: "../image/produtos/shopee/101-150/PRODUTO_N_103.webp",    
+    affiliateLink: "https://s.shopee.com.br/8KeF1cIcOk" 
+  },
+    { 
+    id: "104", 
+    name: "Caixa de Som PC Gamer Com Luz RGB Estéreo ",   
+    image: "../image/produtos/shopee/101-150/PRODUTO_N_104.webp",    
+    affiliateLink: "https://s.shopee.com.br/1BB4PyPjbS" 
+  },
+]
