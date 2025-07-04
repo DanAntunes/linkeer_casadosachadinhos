fd7f8d86616deb6c5faa2416db4005676645218a
Soren <danilo-258@hotmail.com>
2025-07-04 17:07:56 -0300

    fix(shopee): correct formatting and indentation for product entries

--- src/assets/js/product/shopee3.js
+++ src/assets/js/product/shopee3.js
@@ -11,20 +11,26 @@ export const productsShopee3 = [
     image: "../image/produtos/shopee/101-150/PRODUTO_N_101.webp",    
     affiliateLink: "https://s.shopee.com.br/8AKoluu1JY" 
   },
-    { 
+  { 
     id: "102", 
     name: "Teclado Gamer/Escritório Com Fio RGB LED ",   
     image: "../image/produtos/shopee/101-150/PRODUTO_N_102.webp",    
     affiliateLink: "https://s.shopee.com.br/4fkwc9lyko" 
   },
-    { 
+  { 
     id: "103", 
     name: "Mouse Gamer Para Jogo 2.400 DPI Óptico com LED RGB",   
     image: "../image/produtos/shopee/101-150/PRODUTO_N_103.webp",    
     affiliateLink: "https://s.shopee.com.br/8KeF1cIcOk" 
   },
-    { 
+  { 
     id: "104", 
+    name: "Caixa de Som PC Gamer Com Luz RGB Estéreo ",   
+    image: "../image/produtos/shopee/101-150/PRODUTO_N_104.webp",    
+    affiliateLink: "https://s.shopee.com.br/1BB4PyPjbS" 
+  },
+  { 
+    id: "105", 
     name: "Caixa de Som PC Gamer Com Luz RGB Estéreo ",   
     image: "../image/produtos/shopee/101-150/PRODUTO_N_104.webp",    
     affiliateLink: "https://s.shopee.com.br/1BB4PyPjbS" 
