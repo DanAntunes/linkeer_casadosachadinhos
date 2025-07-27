25c8a1c96cffd2b9d50cec482abf758bfe4225d4
Soren <danilo-258@hotmail.com>
2025-07-27 17:47:34 -0300

    adicionando produtos 151 a 153

---
+++ src/assets/js/product/shopee4.js
@@ -0,0 +1,20 @@
+export const productsShopee4 = [
+  { 
+    id: "151", 
+    name: "Cola para Sobrancelhas BROWGLUE Transparente à prova d'água",   
+    image: "../image/produtos/shopee/151-200/PRODUTO_N_151.webp",    
+    affiliateLink: "https://s.shopee.com.br/10sGzOrTgr" 
+  },
+  { 
+    id: "152", 
+    name: "Conjunto De Pincéis De Maquiagem De 7 Peças , Blush , Pó Solto , Corretivo , Pincel De Sombra",   
+    image: "../image/produtos/shopee/151-200/PRODUTO_N_152.webp",    
+    affiliateLink: "https://s.shopee.com.br/6KtnLMBXHx" 
+  },
+  { 
+    id: "153", 
+    name: "Pequeno pote de mel óleo labial hidratante hidratante proteção",   
+    image: "../image/produtos/shopee/151-200/PRODUTO_N_153.webp",    
+    affiliateLink: "https://s.shopee.com.br/1VoXalqdcs" 
+  }, 
+]
